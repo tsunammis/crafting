ef3d3ad28c4576780c6b0e4390db315e7a207ae0
Stan Chollet <stanislas.chollet@gmail.com>
2016-04-24 22:13:33 +0200

    generic course

---
+++ learn/linked_list.py
@@ -0,0 +1,15 @@
+"""
+A linked list is a data structure that represents a sequence of nodes.
+In a singly linked list, each node points to the next node in the linked list.
+A doubly linked list gives each node pointers to both the next node and the
+previous node.
+
+Unlike an array, a linked list doest not provide constant time access to a
+particular "index" within the list.
+This means that if you would like to find the Kth element in the list, you will
+need to iterate through K elements.
+
+The benefit of a linked list is that you can add an remove items from the
+beginning of the list in constant time. For specific applications, this can be
+useful.
+"""
